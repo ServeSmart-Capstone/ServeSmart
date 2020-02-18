299a34f700ff9b9861c5ccfa27a4c7306df9f7f8
WantaSpud <spencer.heald@gmail.com>
2020-02-18 11:38:38 -0600

    Started basic tasks object

---
+++ src/models/tasks.js
@@ -0,0 +1,25 @@
+export const orders = [
+    {
+      id: 1,
+      task: [
+        {
+          seat: 1,
+          table: 1,
+          item: 'Bud Light 12oz Draft',
+          price: 3.99,
+        },
+        {
+          seat: 1,
+          item: 'Water',
+          price: 0.0,
+        },
+        {
+          seat: 1,
+          item: 'Pork Chop 8oz',
+          price: 16.99,
+          instructions: ['Medium-well', 'Extra potatoes'],
+        },
+      ],
+    },
+  ];
+  
